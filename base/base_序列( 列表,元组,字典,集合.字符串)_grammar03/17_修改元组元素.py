b640923f8c732839bd7c6e6ca2e923a8cb2600a3
guilin <2469671909@qq.com>
2023-03-27 15:27:50 +0800

    '元组的访问'

--- base/base_序列( 列表,元组,字典,集合.字符串)_grammar03/17_修改元组元素.py
+++ base/base_序列( 列表,元组,字典,集合.字符串)_grammar03/17_修改元组元素.py
@@ -9,22 +9,19 @@
 # @IDE     : PyCharm
 
 
-# 修改元组元素, 使用索引修改元组元素
+# 修改元组元素, 使用索引修改元组元素 不能对其单个元素进行修改
 coffeename = ('蓝山', '卡布奇诺', '曼特宁', '摩卡', '麝香猫', '哥伦比亚')
-# coffeename[4] = "拿铁"    这样写会报错, 因为元组是不可修改序列,不能对其单个元素进行修改
+# coffeename[4] = "拿铁"   # 这样写会报错, 因为元组是不可修改序列,不能对其单个元素进行修改
 # print(coffeename)
 # 1.对整个元组重新赋值
-coffeename = ('蓝山', '卡布奇诺', '曼特宁', '摩卡', '麝香猫', '哥伦比亚')
-coffeename = ('蓝山', '卡布奇诺', '曼特宁', '摩卡', '拿铁', '哥伦比亚')
-print(coffeename)
+coffeename = ('蓝山', '卡布奇诺', '曼特宁', '摩卡', '麝香猫', '哥伦比亚')  # 替换前
+coffeename = ('蓝山', '卡布奇诺', '曼特宁', '摩卡', '拿铁', '哥伦比亚')  # 替换后
+print("元组修改 不能对其单个元素进行修改 只能对整个元组重新赋值： " + str(coffeename))
 
 # 2.还可以队员组进行连接组合
 coffeename1 = ('蓝山', '卡布奇诺', '曼特宁', '摩卡', '麝香猫', '哥伦比亚')
 coffeename1 = ('蓝山', '卡布奇诺', '曼特宁', '摩卡', '拿铁', '哥伦比亚')  # 对上面元组进行重新赋值,删除'麝香猫'
 newcoffeename = ('麝香猫', '维也纳')  # 定义一个新的元组用来元组重组
-allcoffeename = coffeename1 + newcoffeename  # 我们可以进行元组的相加, 但是不能是元组和字符串相加
-print(allcoffeename)
-# 我们可以进行元组的相加, 但是不能是元组和字符串相加
-# print("合并后的:"+allcoffeename)    这样就会报错
-
+allcoffeename = coffeename1 + newcoffeename  # 我们可以进行元组的相加
+print("进行元组的相加 合并: " + str(allcoffeename))
 
