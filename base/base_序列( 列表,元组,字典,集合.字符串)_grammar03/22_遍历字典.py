bd1669a90aa128d3436b90ae72f10e97e6762562
guilin <2469671909@qq.com>
2023-03-28 08:53:56 +0800

    '字典的遍历'

--- base/base_序列( 列表,元组,字典,集合.字符串)_grammar03/22_遍历字典.py
+++ base/base_序列( 列表,元组,字典,集合.字符串)_grammar03/22_遍历字典.py
@@ -13,17 +13,19 @@
 # 遍历:对所有的内容进行一遍访问,对于字典以键值对的形式存储数据,所以可能用到获取这些键值对,Python提供的遍历字典的方法:items
 sign = {'绮梦': '水瓶座', '冷伊一': '射手座', '香凝': '双鱼座', '黛兰': '双子座'}  # 首先定义一个字典
 sign.items()  # 返回的是一个可以遍历的列表,每一个列表的元素都是一个元组
-print("看一下输出结果法1: ", sign.items())
+print("遍历字典 .items() 输出结果 \n ", sign.items())
+print('\n')  # 仅仅起一个分隔的作用
 for item in sign.items():
-    print("看一下输出结果方法2:", item)  # 就是每一个循环中都会带一个"看一下输出结果方法2:"
+    print("for循环遍历字典 输出结果 ", item)
 
 # 如果我们不想得到元组的形式(),想要得到每个键和具体的值的时候:
+print('\n')  # 仅仅起一个分隔的作用
 for key, value in sign.items():
-    print("不想要元组形式的输出结果值:", key, "的星座是:", value)
-
+    print("得到具体的键和值 输出结果值: ", key, "的星座是:", value)
+print('\n')  # 仅仅起一个分隔的作用
 # 得到字典中的键列表,或者是值列表
 for key in sign.keys():
-    print(key)  # 得到字典中的键列表
+    print("得到字典中的键列表  " + str(key))  # 得到字典中的键列表
 print('\n')  # 仅仅起一个分隔的作用
 for value in sign.values():
-    print(value)  # 得到字典中的值列表
+    print("得到字典中的值列表 " + value)  # 得到字典中的值列表
